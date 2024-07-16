dbd4aa55ea239f37449d471da3efc86207c5d722
Sitruc555 <Curtisneasmith504@gmail.com>
2024-07-16 17:40:07 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##There are two functions makeCachematrix, makeCacheMatrix
+##makeCacheMatrix consists of set, get, setinv, getinv
+##library(MASS) is used to calculate inverse for non sqaured as well as sqaured matrices 
+library(MASS) 
+makeCacheMatrix <- function (x = matrix()) { 
+  inv<- NULL
+  set <- function(y){
+                    x<<- y 
+                    inv<<-NULL
+                    }
+  get<-function ()x                       #function to get matrix x 
+  setinv<-function(inverse)inv<<-inverse 
+  getinv<-function(){
+                    inver<-ginv(x)
+                    inver%*% x              #function to obtain inverse of the matrix 
+                    } 
+  
+  list(set = set, get = get, 
+       setinv = setinv, 
+       getinv = getinv) 
+} 
 
-## Write a short comment describing this function
+##This is used to get the cache data 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve <- function(x, ...) ## gets cache data 
+{
+  inv<-x$getinv()
+  if(!is.null(inv)){                           #checking whether inverse is NULL
+                            message("getting cached data!") 
+                            return(inv)        #returns inverse value 
+  }
+  data<-x$get()
+  inv<-solve(data...) 
+  x$set(inv) 
+  inv       ##Return a matrix that is the inverse of 'x'
 }
+    
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
